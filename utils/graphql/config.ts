90f730a217a8954a1e72466a675e991a6d4f5690
Ezekiel Victor <codeepoch@gmail.com>
2022-12-08 19:33:50 +0100

    added apollo-client to app

---
+++ utils/graphql/config.ts
@@ -0,0 +1,7 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const ApiUrl = process.env.NEXT_PUBLIC_API_URL
+export const client = new ApolloClient({
+    uri:  ApiUrl,
+    cache: new InMemoryCache()
+})

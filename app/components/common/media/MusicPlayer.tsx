0d8f579e17e56a969db79e64e57fc67c952c8286
Ezekiel Victor <codeepoch@gmail.com>
2022-12-24 21:41:19 +0100

    started with podcast player

--- app/components/common/media/MusicPlayer.tsx
+++ app/components/common/media/MusicPlayer.tsx
@@ -1,12 +1,53 @@
+import React from "react";
 import { useAppSelector } from "@utils/store/hooks";
 import { selectPlayer } from "@utils/store/slices/playerSlice";
-import React from "react";
+import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
 
 const MusicPlayer = React.forwardRef<HTMLDivElement>((props, ref) => {
+  const [playing, setPlaying] = React.useState(false);
   const player = useAppSelector(selectPlayer);
+  const audioRef = React.useRef() as React.MutableRefObject<HTMLAudioElement>;
+  const [audio, setAudio] = React.useState({
+    duration: "0.0"
+  });
+
+  function audioPlayHandler(): void {
+    if (playing) {
+      audioRef.current.pause();
+      setPlaying(false);
+    } else {
+      void audioRef.current.play();
+      setPlaying(true);
+    }
+  }
+
+  React.useEffect(() => {
+    console.log(audioRef.current);
+    const duration = (audioRef.current.duration / 60).toFixed(2);
+    setAudio((curr) => ({ ...curr, duration }));
+  }, []);
+
   return (
-    <div className={`${player.color?.lighter ?? ""} p-5 fixed bottom-0 w-screen`} ref={ref}>
-      <audio src="/music/Toxic.mp3" controls />
+    <div
+      className={`${
+        player.color?.lighter ?? ""
+      } p-3 fixed bottom-2 left-1/2 shadow-md shadow-black/80
+      flex items-center gap-4
+      -translate-x-1/2 w-[97vw] sm:max-w-[90vw] rounded-2xl`}
+      ref={ref}
+    >
+      <button
+        onClick={audioPlayHandler}
+        className={`
+        text-5xl text-white ${player.color?.darker ?? ""}
+        aspect-square rounded-full flex items-center justify-center shadow-md`}
+      >
+        {playing ? <BsFillPauseFill /> : <BsFillPlayFill />}
+      </button>
+      <div>
+        <time>{audio.duration}</time>
+      </div>
+      <audio className="hiden" ref={audioRef} src="/music/Toxic.mp3" />
     </div>
   );
 });
